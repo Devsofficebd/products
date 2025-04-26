6d21e2f12529fc9394b107eea97e19ed4c79dadf
Ashiq Hosen <142709619+Devsofficebd@users.noreply.github.com>
2025-04-26 08:36:50 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,35 @@
+const defaultBoxes = [
+    {
+      name: "LearnDash Project",
+      description: "A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.",
+      links: ["https://example.com/learndash"],
+      price: 100,
+      discount: 80
+    },
+    {
+      name: "WooCommerce Store",
+      description: "An eCommerce plugin for WordPressA learning management system for WordPress.A learning management system for WordPress.A learning management system fo..",
+      links: ["https://example.com/woocommerce"],
+      price: 50
+    },
+    {
+        name: "LearnDash Project",
+        description: "A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.",
+        links: ["https://example.com/learndash"],
+        price: 100,
+        discount: 80
+      },    {
+        name: "LearnDash Project",
+        description: "A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.",
+        links: ["https://example.com/learndash"],
+        price: 100,
+        discount: 80
+      },    {
+        name: "LearnDash Project",
+        description: "A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.A learning management system for WordPress.",
+        links: ["https://example.com/learndash"],
+        price: 100,
+        discount: 80
+      },
+  ];
+  
